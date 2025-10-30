498005b8d59d451177c98ee80123df5ff4d1b153
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-30 16:18:59 -0300

    refactor: ensure imageUrl is nullabe and optional

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,10 +1,14 @@
-import { z } from "zod";
+import { z } from "zod/v4";
 
 export const createUserSchema = z.object({
-  first_name: z.string().min(1),
-  last_name: z.string().min(1),
-  email: z.string().email(),
-  password: z.string().min(6),
+  first_name: z.string().trim().min(1, "First name is required."),
+  last_name: z.string().min(1, "Last name is required."),
+  imageUrl: z.url().optional().nullable(),
+  email: z.email().min(1, "E-mail is required."),
+  password: z
+    .string()
+    .trim()
+    .min(6, "Password must have at least 6 characters."),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
