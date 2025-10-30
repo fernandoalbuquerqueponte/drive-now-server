1cfb283b7ec7815fe56cb84fd8b64135ba90db91
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-10-30 16:21:19 -0300

    fix: ensure imageUrl is retorned

--- src/repositories/postgres/user/create-user.ts
+++ src/repositories/postgres/user/create-user.ts
@@ -1,16 +1,21 @@
 import prismaClient from "../../../../prisma/prisma.js";
+import type { CreateUserSchema } from "../../../schemas/user.js";
+import type { ICreateUsersRepository } from "../../../types/user.js";
 
-interface CreateUserType {
-  first_name: string;
-  last_name: string;
-  email: string;
-  password: string;
-}
+type CreateUserRepositoryParams = Omit<CreateUserSchema, "password"> & {
+  hashedPassword: string;
+};
 
-export class PostgresCreateUserRepository {
-  async execute(params: CreateUserType) {
+export class PostgresCreateUserRepository implements ICreateUsersRepository {
+  async execute(params: CreateUserRepositoryParams) {
     const user = await prismaClient.user.create({
-      data: params,
+      data: {
+        first_name: params.first_name,
+        last_name: params.last_name,
+        email: params.email,
+        imageUrl: params.imageUrl ?? null,
+        password: params.hashedPassword,
+      },
     });
 
     return user;
