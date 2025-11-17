65849ace9f1580bf7fb13d9e945f9b3c24a8b887
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-17 14:40:28 -0300

    test: ensure GetUserByIdController returns user successfully

---
+++ src/tests/controllers/user/get-user-by-id.test.ts
@@ -0,0 +1,40 @@
+import { faker } from "@faker-js/faker";
+import { GetUserByIdController } from "../../../controllers/index.js";
+import type { User } from "../../../types/user.js";
+import { GetUserByIdUseCase } from "../../../use-cases/index.js";
+import type { Request } from "express";
+import type { CreateUserSchema } from "../../../schemas/user.js";
+
+describe("GetUserByIdController", () => {
+  class GetUserByIdUseCaseStub {
+    async execute(): Promise<User> {
+      return {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const getUserByIdUseCase =
+      new GetUserByIdUseCaseStub() as unknown as GetUserByIdUseCase;
+    const sut = new GetUserByIdController(getUserByIdUseCase);
+
+    return { sut };
+  };
+
+  const httpRequest = {
+    params: {
+      userId: faker.string.uuid(),
+    },
+  } as Request<any, any, CreateUserSchema>;
+
+  it("should return user successfully", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute(httpRequest);
+
+    expect(response.statusCode).toBe(200);
+  });
+});
