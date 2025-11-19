ae847a034362175f5b5fed73e429218019550ce2
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2025-11-19 14:52:02 -0300

    fix: ensure PostgresDeleteUserRepository returns null if user not found

--- src/repositories/postgres/user/delete-user.ts
+++ src/repositories/postgres/user/delete-user.ts
@@ -3,17 +3,14 @@ import type { IDeleteUserRepository } from "../../../types/user.js";
 
 export class PostgresDeleteUserRepository implements IDeleteUserRepository {
   async execute(userId: string) {
-    const deletedUser = await prismaClient.user.delete({
-      where: { id: userId },
-      select: {
-        id: true,
-        first_name: true,
-        last_name: true,
-        email: true,
-        imageUrl: true,
-      },
-    });
+    try {
+      const deletedUser = await prismaClient.user.delete({
+        where: { id: userId },
+      });
 
-    return deletedUser;
+      return deletedUser;
+    } catch (error) {
+      return null;
+    }
   }
 }
