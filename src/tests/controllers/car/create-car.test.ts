11844afb6da73edc02934d2909712d510bf9de25
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 16:03:05 -0300

    test: ensure CreateCarController returns 201 on create car successfully

---
+++ src/tests/controllers/car/create-car.test.ts
@@ -0,0 +1,58 @@
+import { faker } from "@faker-js/faker";
+import { CreateCarController } from "../../../controllers/car/create-car.js";
+import { CreateCarUseCase } from "../../../use-cases/car/create-car.js";
+import type { Request } from "express";
+import type { CreateCarSchema } from "../../../schemas/car.js";
+
+describe("CreateCarController", () => {
+  class CreateCarUseCaseStub {
+    async execute(carData: CreateCarSchema, userId: string) {
+      return {
+        id: faker.string.uuid(),
+        ...carData,
+        user_id: userId,
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const createCarUseCase =
+      new CreateCarUseCaseStub() as unknown as CreateCarUseCase;
+
+    const sut = new CreateCarController(createCarUseCase);
+
+    return {
+      sut,
+      createCarUseCase,
+    };
+  };
+
+  const httpRequest = {
+    params: {
+      userId: faker.string.uuid(),
+    },
+    body: {
+      brand: faker.vehicle.manufacturer(),
+      model: faker.vehicle.model(),
+      category: "SUV",
+      image: faker.image.url(),
+      gallery: [faker.image.url(), faker.image.url()],
+      year: 2022,
+      pricePerHour: 120,
+      description: faker.lorem.paragraph(),
+      specifications: [
+        { label: "Motor", value: "2.0 Flex" },
+        { label: "Câmbio", value: "Automático CVT" },
+      ],
+      features: ["Air Conditioning", "ABS", "GPS"],
+    },
+  } as unknown as Request<any, any, Partial<CreateCarSchema>>;
+
+  it("should create a car successfully", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(201);
+  });
+});
