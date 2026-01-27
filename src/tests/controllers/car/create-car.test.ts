6f957e33154b65d327115cf34051234f2b39c1b3
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 17:20:41 -0300

    test: ensure CreateCarController returns 400 if userId params is not provided

--- src/tests/controllers/car/create-car.test.ts
+++ src/tests/controllers/car/create-car.test.ts
@@ -70,6 +70,19 @@ describe("CreateCarController", () => {
     expect(result.statusCode).toBe(400);
   });
 
+  it("should return 400 if userId is missing", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      params: {
+        userId: null,
+      },
+    } as unknown as Request<any, any, Partial<CreateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
+
   it("should return 400 if specifications are invalid", async () => {
     const { sut } = makeSut();
 
