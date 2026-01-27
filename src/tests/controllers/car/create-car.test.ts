966391d8d778f1a2cf90c8f07b744940df71496b
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 16:56:16 -0300

    test: ensure CreateCarController returns 400 if invalid image is provided

--- src/tests/controllers/car/create-car.test.ts
+++ src/tests/controllers/car/create-car.test.ts
@@ -97,4 +97,18 @@ describe("CreateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if image is invalid", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        image: "invalid_url",
+      },
+    } as unknown as Request<any, any, Partial<CreateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
