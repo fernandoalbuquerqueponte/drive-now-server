d0f447e0bc1f9c47ee36bc89bab43bcbfe5a5c0a
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 17:26:10 -0300

    test: ensure CreateCarController returns 400 if category is missing

--- src/tests/controllers/car/create-car.test.ts
+++ src/tests/controllers/car/create-car.test.ts
@@ -166,4 +166,18 @@ describe("CreateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if category is missing", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        category: undefined,
+      },
+    } as unknown as Request<any, any, Partial<CreateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
