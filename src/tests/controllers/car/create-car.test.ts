89d64bf7fce4a1aab7e9cb4cc03b963f0b95f6fe
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 17:25:01 -0300

    test: ensure CreateCarController returns 400 if model is missing

--- src/tests/controllers/car/create-car.test.ts
+++ src/tests/controllers/car/create-car.test.ts
@@ -152,4 +152,18 @@ describe("CreateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if model is missing", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        model: undefined,
+      },
+    } as unknown as Request<any, any, Partial<CreateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
