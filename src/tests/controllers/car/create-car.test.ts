a04868559ea2897e4ce7d78c6af06f30ebe94a0b
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-27 17:27:15 -0300

    test: ensure CreateCarController returns 400 if year is missing

--- src/tests/controllers/car/create-car.test.ts
+++ src/tests/controllers/car/create-car.test.ts
@@ -180,4 +180,18 @@ describe("CreateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if year is missing", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        year: undefined,
+      },
+    } as unknown as Request<any, any, Partial<CreateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
