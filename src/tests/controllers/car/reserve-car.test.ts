37aa3881898b0669acb0328b5295ccc2f7c375fb
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-29 08:57:54 -0300

    test: ensure ReserveCarController returns 400 if endDate is missing

--- src/tests/controllers/car/reserve-car.test.ts
+++ src/tests/controllers/car/reserve-car.test.ts
@@ -73,6 +73,19 @@ describe("ReserveCarController", () => {
     const result = await sut.execute({
       ...httpRequest,
       body: {
+        startDate: undefined,
+      },
+    } as unknown as Request<any, any, Partial<ReserveCarInputDTO>>);
+
+    expect(result.statusCode).toBe(400);
+  });
+
+  it("should return 400 if endDate is missing", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
         endDate: undefined,
       },
     } as unknown as Request<any, any, Partial<ReserveCarInputDTO>>);
