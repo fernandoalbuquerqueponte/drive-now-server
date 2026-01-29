671dcd75d075a02b425a58ecb15f6247702cf9ff
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-29 15:22:08 -0300

    test: ensure UpdateCarController returns 400 when invalid specifications are provided

--- src/tests/controllers/car/update-car.test.ts
+++ src/tests/controllers/car/update-car.test.ts
@@ -89,4 +89,18 @@ describe("UpdateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 when invalid specifications is provided", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        specifications: [{ label: "Motor" }],
+      },
+    } as unknown as Request<any, any, Partial<UpdateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
