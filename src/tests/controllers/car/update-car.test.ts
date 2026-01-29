4b8080afdfa0d72eb3c7adb0ddacd2dac6b273ea
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-29 11:14:59 -0300

    test: ensure UpdateCarController return 200 when car is updated

---
+++ src/tests/controllers/car/update-car.test.ts
@@ -0,0 +1,40 @@
+import type { Request } from "express";
+import { UpdateCarController } from "../../../controllers/car/update-car.js";
+import type { UpdateCarUseCase } from "../../../use-cases/car/update-car.js";
+import type { UpdateCarSchema } from "../../../schemas/car.js";
+import { faker } from "@faker-js/faker";
+import { car } from "../../fixtures/index.js";
+
+describe("UpdateCarController", () => {
+  class UpdateCarUseCaseStub {
+    async execute() {
+      return car;
+    }
+  }
+
+  const httpRequest = {
+    params: {
+      carId: faker.string.uuid(),
+      userId: faker.string.uuid(),
+    },
+    body: {
+      car,
+    },
+  } as unknown as Request<any, any, Partial<UpdateCarSchema>>;
+
+  const makeSut = () => {
+    const updateCarUseCase =
+      new UpdateCarUseCaseStub() as unknown as UpdateCarUseCase;
+    const sut = new UpdateCarController(updateCarUseCase);
+
+    return { sut, updateCarUseCase };
+  };
+
+  it("should update a car successfully", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(httpRequest);
+
+    expect(result.statusCode).toBe(200);
+  });
+});
