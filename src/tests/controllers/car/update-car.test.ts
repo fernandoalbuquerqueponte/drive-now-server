0d1f2bba28f00f7bbb772ce77ac5f34bc3eb9ea7
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-29 15:23:32 -0300

    test: ensure UpdateCarController returns 400 if gallery is invalid

--- src/tests/controllers/car/update-car.test.ts
+++ src/tests/controllers/car/update-car.test.ts
@@ -131,4 +131,18 @@ describe("UpdateCarController", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if gallery is invalid", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute({
+      ...httpRequest,
+      body: {
+        ...httpRequest.body,
+        gallery: ["invalid_url1", "invalid_url2"],
+      },
+    } as unknown as Request<any, any, Partial<UpdateCarSchema>>);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
