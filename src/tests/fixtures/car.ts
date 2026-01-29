1b751c9e5f6c1dcbc32786fc9f7d110a714a758a
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-01-29 11:14:35 -0300

    feat: add car fixture

---
+++ src/tests/fixtures/car.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+import type { CreateCarSchema } from "../../schemas/car.js";
+
+export const car: CreateCarSchema = {
+  brand: faker.vehicle.manufacturer(),
+  model: faker.vehicle.model(),
+  category: "SUV",
+  image: faker.image.url(),
+  gallery: [faker.image.url(), faker.image.url()],
+  year: 2022,
+  pricePerHour: 120,
+  description: faker.lorem.paragraph(),
+  specifications: [
+    { label: "Motor", value: "2.0 Flex" },
+    { label: "Câmbio", value: "Automático CVT" },
+  ],
+  features: ["Air Conditioning", "ABS", "GPS"],
+};
