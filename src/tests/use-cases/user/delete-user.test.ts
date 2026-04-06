077ebb4c2f019aabaade71aef824bb50691b881e
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-06 15:01:39 -0300

    test: ensure DeleteUserUseCase delete an user successfully

---
+++ src/tests/use-cases/user/delete-user.test.ts
@@ -0,0 +1,30 @@
+import type { IDeleteUserRepository } from "../../../types/user.js";
+import { DeleteUserUseCase } from "../../../use-cases/index.js";
+import { user } from "../../fixtures/user.js";
+
+describe("DeleteUserUseCase", () => {
+  class DeleteUserRepositoryStub implements IDeleteUserRepository {
+    async execute() {
+      return user;
+    }
+  }
+
+  const makeSut = () => {
+    const deleteUserRepository = new DeleteUserRepositoryStub();
+    const sut = new DeleteUserUseCase(deleteUserRepository);
+
+    return {
+      sut,
+      deleteUserRepository,
+    };
+  };
+
+  it("should delete a user", async () => {
+    const { sut } = makeSut();
+
+    const deletedUser = await sut.execute(user.id);
+
+    expect(deletedUser).toBeTruthy();
+    expect(deletedUser).toEqual(user);
+  });
+});
