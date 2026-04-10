03c5b20a17c4c9d4424d441a7b8230a12d699704
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-10 15:59:13 -0300

    test: ensure GetCarReviewsUseCase returns the reviews

---
+++ src/tests/use-cases/car/get-car-reviews.test.ts
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker";
+import type {
+  IGetCarReviewsRepository,
+  IPostgresGetCarByIdRepository,
+} from "../../../types/car.js";
+import { GetCarReviewsUseCase } from "../../../use-cases/car/get-car-reviews.js";
+import { review } from "../../fixtures/car.js";
+
+describe("GetCarReviewsUseCase", () => {
+  const carId = faker.string.uuid();
+
+  class GetCarReviewsRepositoryStub {
+    async execute() {
+      return review;
+    }
+  }
+
+  class GetCarByIdRepositoryStub {
+    async execute() {
+      return carId;
+    }
+  }
+
+  const makeSut = () => {
+    const getCarReviewsRepositoryStub =
+      new GetCarReviewsRepositoryStub() as unknown as IGetCarReviewsRepository;
+
+    const getCarByIdRepositoryStub =
+      new GetCarByIdRepositoryStub() as unknown as IPostgresGetCarByIdRepository;
+
+    const sut = new GetCarReviewsUseCase(
+      getCarReviewsRepositoryStub,
+      getCarByIdRepositoryStub,
+    );
+
+    return { getCarReviewsRepositoryStub, getCarByIdRepositoryStub, sut };
+  };
+
+  it("should return car reviews", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(carId);
+
+    expect(result).toEqual(review);
+  });
+});
