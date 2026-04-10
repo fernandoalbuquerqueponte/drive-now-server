c516fc1a7b4ecf8a692e68e7b626fbe8cb3994c1
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-10 19:10:22 -0300

    test: ensure ReserveCarUseCase booking a car successfully

---
+++ src/tests/use-cases/car/reserve-car.test.ts
@@ -0,0 +1,57 @@
+import { faker } from "@faker-js/faker";
+import { booking } from "../../fixtures/car.js";
+import { ReserveCarUseCase } from "../../../use-cases/car/reserve-car.js";
+import type {
+  IPostgresGetCarByIdRepository,
+  IPostgresReserveCarRepository,
+} from "../../../types/car.js";
+import { user } from "../../fixtures/user.js";
+
+describe("ReserveCarUseCase", () => {
+  const carId = faker.string.uuid();
+  const startDate = faker.date.soon();
+  const endDate = faker.date.future();
+
+  const params = { startDate, endDate };
+
+  class ReserveCarRepositoryStub {
+    async execute() {
+      return {
+        ...booking,
+        carId: carId,
+        userId: user.id,
+      };
+    }
+  }
+
+  class GetCarByIdRepositoryStub {
+    async execute() {
+      return carId;
+    }
+  }
+
+  const makeSut = () => {
+    const reserveCarRepository =
+      new ReserveCarRepositoryStub() as unknown as IPostgresReserveCarRepository;
+
+    const getCarByIdRepository =
+      new GetCarByIdRepositoryStub() as unknown as IPostgresGetCarByIdRepository;
+
+    const sut = new ReserveCarUseCase(
+      reserveCarRepository,
+      getCarByIdRepository,
+    );
+
+    return { reserveCarRepository, getCarByIdRepository, sut };
+  };
+
+  it("should booking a car successfully", async () => {
+    const { sut } = makeSut();
+
+    const bookingCar = await sut.execute(carId, user.id, params);
+
+    expect(bookingCar).toBeTruthy();
+    expect(bookingCar).toHaveProperty("id");
+    expect(bookingCar.carId).toBe(carId);
+  });
+});
