b1d28748cc8d9011f9f24b38269ba0e9f5e136b4
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-10 15:55:25 -0300

    feat: implement car existence check in GetCarReviewsUseCase

--- src/use-cases/car/get-car-reviews.ts
+++ src/use-cases/car/get-car-reviews.ts
@@ -1,11 +1,25 @@
-import type { IGetCarReviewsRepository } from "../../types/car.js";
+import { CarNotFoundError } from "../../errors/car.js";
+import type {
+  IGetCarReviewsRepository,
+  IPostgresGetCarByIdRepository,
+} from "../../types/car.js";
 
 export class GetCarReviewsUseCase {
-  constructor(private getCarReviewsRepository: IGetCarReviewsRepository) {
+  constructor(
+    private getCarReviewsRepository: IGetCarReviewsRepository,
+    private getCarByIdRepository: IPostgresGetCarByIdRepository,
+  ) {
     this.getCarReviewsRepository = getCarReviewsRepository;
+    this.getCarByIdRepository = getCarByIdRepository;
   }
 
   async execute(carId: string) {
+    const carExists = await this.getCarByIdRepository.execute(carId);
+
+    if (!carExists) {
+      throw new CarNotFoundError();
+    }
+
     const carReviews = await this.getCarReviewsRepository.execute(carId);
 
     return carReviews;
