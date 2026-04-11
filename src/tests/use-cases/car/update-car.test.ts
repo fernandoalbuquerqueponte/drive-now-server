264a92d26943eb98dc4fc559d128059c07ac618f
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-11 18:43:04 -0300

    test: ensure UpdateCarUseCase updates a car successfully

---
+++ src/tests/use-cases/car/update-car.test.ts
@@ -0,0 +1,59 @@
+import { faker } from "@faker-js/faker";
+import type {
+  IPostgresGetCarByIdRepository,
+  IPostgresUpdateCarRepository,
+} from "../../../types/car.js";
+import { UpdateCarUseCase } from "../../../use-cases/car/update-car.js";
+import { car } from "../../fixtures/car.js";
+
+describe("UpdateCarUseCase", () => {
+  const carId = faker.string.uuid();
+  const userId = faker.string.uuid();
+
+  const carWithUserId = {
+    ...car,
+    id: carId,
+    user_id: userId,
+  };
+
+  class UpdateCarRepositoryStub {
+    async execute() {
+      return carWithUserId;
+    }
+  }
+
+  class GetCarByIdRepositoryStub {
+    async execute() {
+      return carWithUserId;
+    }
+  }
+
+  const makeSut = () => {
+    const updateCarRepositoryStub =
+      new UpdateCarRepositoryStub() as unknown as IPostgresUpdateCarRepository;
+    const getCarByIdRepositoryStub =
+      new GetCarByIdRepositoryStub() as unknown as IPostgresGetCarByIdRepository;
+
+    const sut = new UpdateCarUseCase(
+      updateCarRepositoryStub,
+      getCarByIdRepositoryStub,
+    );
+    return {
+      sut,
+      updateCarRepositoryStub,
+      getCarByIdRepositoryStub,
+    };
+  };
+
+  it("should update a car successfully", async () => {
+    const { sut } = makeSut();
+
+    const updatedCar = await sut.execute(
+      faker.string.uuid(),
+      car,
+      carWithUserId.user_id,
+    );
+
+    expect(updatedCar).toBe(carWithUserId);
+  });
+});
