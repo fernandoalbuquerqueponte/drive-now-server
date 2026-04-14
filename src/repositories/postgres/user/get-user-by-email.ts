1b9401a85e3ae9b6bda6db1efedaaf105cebd0e4
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-14 17:00:56 -0300

    feat: ensure returns null when user is not found

--- src/repositories/postgres/user/get-user-by-email.ts
+++ src/repositories/postgres/user/get-user-by-email.ts
@@ -1,13 +1,15 @@
 import prismaClient from "../../../../prisma/prisma.js";
 import type { IGetUserByEmailRepository } from "../../../types/user.js";
 
-export class PostgresEmailIsAlreadyInUseUserRepository
-  implements IGetUserByEmailRepository
-{
+export class PostgresEmailIsAlreadyInUseUserRepository implements IGetUserByEmailRepository {
   async execute(email: string) {
     const response = await prismaClient.user.findFirst({
       where: { email },
     });
+
+    if (!response) {
+      return null;
+    }
 
     return response;
   }
