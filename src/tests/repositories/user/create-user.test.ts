1b9921d1f77b5036c5da7e3c4e4ba566a229e345
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-14 14:58:15 -0300

    test: ensure PostgresCreateUserRepository call prisma with correct params

--- src/tests/repositories/user/create-user.test.ts
+++ src/tests/repositories/user/create-user.test.ts
@@ -1,15 +1,36 @@
+import prismaClient from "../../../../prisma/prisma.js";
 import { PostgresCreateUserRepository } from "../../../repositories/postgres/index.js";
 import { user } from "../../fixtures/user.js";
 
 describe("CreateUserRepository", () => {
+  const userWithoutId = { ...user, id: undefined };
+
   it("should create an user on db", async () => {
     const sut = new PostgresCreateUserRepository();
 
-    const result = await sut.execute(user);
+    const result = await sut.execute(userWithoutId);
 
-    expect(result.first_name).toBe(user.first_name);
-    expect(result.last_name).toBe(user.last_name);
-    expect(result.email).toBe(user.email);
-    expect(result.imageUrl).toBe(user.imageUrl);
+    expect(result.first_name).toBe(userWithoutId.first_name);
+    expect(result.last_name).toBe(userWithoutId.last_name);
+    expect(result.email).toBe(userWithoutId.email);
+    expect(result.imageUrl).toBe(userWithoutId.imageUrl);
+  });
+
+  it("should call prisma with correct params", () => {
+    const sut = new PostgresCreateUserRepository();
+    const executeSpy = jest.spyOn(prismaClient.user, "create");
+
+    sut.execute(userWithoutId);
+
+    expect(executeSpy).toHaveBeenCalledWith({
+      data: userWithoutId,
+      select: {
+        id: true,
+        first_name: true,
+        last_name: true,
+        email: true,
+        imageUrl: true,
+      },
+    });
   });
 });
