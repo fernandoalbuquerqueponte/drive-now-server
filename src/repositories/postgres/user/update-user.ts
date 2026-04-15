eadee832b751841d72944e2398a3fe4a9e7359af
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-15 00:52:25 -0300

    refactor: simplify user update logic in PostgresUpdateUserRepository

--- src/repositories/postgres/user/update-user.ts
+++ src/repositories/postgres/user/update-user.ts
@@ -1,23 +1,34 @@
 import prismaClient from "../../../../prisma/prisma.js";
 import type { IUpdateUserRepository } from "../../../types/user.js";
 import type { UpdateUserSchema, User } from "../../../schemas/user.js";
+import { Prisma } from "@prisma/client";
 
 export class PostgresUpdateUserRepository implements IUpdateUserRepository {
   async execute(
     userId: string,
-    params: UpdateUserSchema
+    params: UpdateUserSchema,
   ): Promise<User | null> {
-    const data = Object.fromEntries(
-      Object.entries(params).filter(([_, v]) => v !== undefined)
-    );
+    try {
+      const updatedUser = await prismaClient.user.update({
+        where: { id: userId },
+        data: params,
+        select: {
+          id: true,
+          first_name: true,
+          last_name: true,
+          email: true,
+          imageUrl: true,
+        },
+      });
 
-    const updatedUser = await prismaClient.user.update({
-      where: { id: userId },
-      data: data,
-    });
-
-    const { password, ...safeUser } = updatedUser;
-
-    return safeUser;
+      return updatedUser;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          return null;
+        }
+      }
+      throw error;
+    }
   }
 }
