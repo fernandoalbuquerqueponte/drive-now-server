3d6526959d9ed610810202c9627ae88fe004f4f0
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-16 16:25:41 -0300

    test: add error handling for car deletion in DeleteCarRepository

--- src/tests/repositories/car/delete-car.test.ts
+++ src/tests/repositories/car/delete-car.test.ts
@@ -2,6 +2,8 @@ import { PostgresDeleteCarRepository } from "../../../repositories/postgres/car/
 import { car } from "../../fixtures/car.js";
 import prismaClient from "../../../../prisma/prisma.js";
 import { user } from "../../fixtures/user.js";
+import { Prisma } from "@prisma/client";
+import { CarNotFoundError } from "../../../errors/car.js";
 
 describe("DeleteCarRepository", () => {
   it("should delete a car sucessfully", async () => {
@@ -50,6 +52,44 @@ describe("DeleteCarRepository", () => {
   it("should throw a generic error", async () => {
     const sut = new PostgresDeleteCarRepository();
     jest.spyOn(prismaClient.car, "delete").mockRejectedValueOnce(new Error());
+
+    const promise = sut.execute("any-id");
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it("should throw CarNotFoundError if car is not found", async () => {
+    // await prismaClient.user.create({ data: user });
+    // const createdCar = await prismaClient.car.create({
+    //   data: {
+    //     ...car,
+    //     user_id: user.id,
+    //   },
+    // });
+
+    const sut = new PostgresDeleteCarRepository();
+
+    // jest.spyOn(prismaClient.car, "delete").mockRejectedValueOnce(
+    //   new Prisma.PrismaClientKnownRequestError("Record to update not found.", {
+    //     code: "P2025",
+    //     clientVersion: "5.x",
+    //   }),
+    // );
+
+    await expect(sut.execute("invalidId")).rejects.toThrow(
+      new CarNotFoundError(),
+    );
+  });
+
+  it("should throw error if prisma throws a KnownRequestError with a code different from P2025", async () => {
+    const sut = new PostgresDeleteCarRepository();
+
+    jest.spyOn(prismaClient.car, "delete").mockRejectedValueOnce(
+      new Prisma.PrismaClientKnownRequestError("Unique constraint failed", {
+        code: "P2002",
+        clientVersion: "5.x",
+      }),
+    );
 
     const promise = sut.execute("any-id");
 
