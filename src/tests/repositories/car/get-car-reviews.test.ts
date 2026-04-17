fc2268d052c876d2ee40ef92b13fce50b0de131d
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-16 21:13:24 -0300

    test: add GetCarReviewsRepository test to verify successful retrieval of car reviews

---
+++ src/tests/repositories/car/get-car-reviews.test.ts
@@ -0,0 +1,32 @@
+import prismaClient from "../../../../prisma/prisma.js";
+import { GetCarReviewsRepository } from "../../../repositories/postgres/car/get-car-reviews.js";
+import { car } from "../../fixtures/car.js";
+import { user } from "../../fixtures/user.js";
+
+describe("GetCarReviewsRepository", () => {
+  it("should get car reviews successfully", async () => {
+    const createdUser = await prismaClient.user.create({ data: user });
+    const createdCar = await prismaClient.car.create({
+      data: {
+        ...car,
+        user_id: createdUser.id,
+      },
+    });
+
+    const reviewData = {
+      comment: "Ótimo carro!",
+      rating: 5,
+      carId: createdCar.id,
+      userId: createdUser.id,
+    };
+
+    await prismaClient.review.create({ data: reviewData });
+
+    const sut = new GetCarReviewsRepository();
+    const result = await sut.execute(createdCar.id);
+
+    expect(result).toBeDefined();
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(1);
+  });
+});
