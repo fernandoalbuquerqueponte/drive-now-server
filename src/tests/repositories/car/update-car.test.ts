72470c2779efc43fae9fc7397a499dc690036b57
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-17 14:29:32 -0300

    test: ensure UpdateCarRepository update a user successfully

---
+++ src/tests/repositories/car/update-car.test.ts
@@ -0,0 +1,29 @@
+import prismaClient from "../../../../prisma/prisma.js";
+import { PostgresUpdateCarRepository } from "../../../repositories/postgres/car/update-car.js";
+import { car } from "../../fixtures/car.js";
+import { user } from "../../fixtures/user.js";
+
+describe("UpdateCarRepository", () => {
+  it("should update car successfully", async () => {
+    await prismaClient.user.create({ data: user });
+    const createdCar = await prismaClient.car.create({
+      data: {
+        ...car,
+        user_id: user.id,
+      },
+    });
+
+    const sut = new PostgresUpdateCarRepository();
+    const updateData = { ...car, brand: "Nome Novo" };
+
+    const result = await sut.execute(createdCar.id, updateData);
+
+    expect(result.id).toBe(createdCar.id);
+    expect(result.brand).toBe("Nome Novo");
+
+    const carInDb = await prismaClient.car.findUnique({
+      where: { id: createdCar.id },
+    });
+    expect(carInDb?.brand).toBe("Nome Novo");
+  });
+});
