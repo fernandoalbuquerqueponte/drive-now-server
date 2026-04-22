e97a67da2e93a8cac76aa395ce4cce75925934b9
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-22 15:08:49 -0300

    test: add end-to-end tests for car creation route

---
+++ src/tests/routes/cars.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+
+import { app } from "../../app.js";
+import { user } from "../fixtures/user.js";
+import { car } from "../fixtures/index.js";
+
+describe("Cars Route E2E Tests", () => {
+  it("POST /api/cars should return 201 when car is created successfully", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const response = await request(app)
+      .post("/api/cars")
+      .set("Authorization", `Bearer ${createdUser.tokens.accessToken}`)
+      .send(car);
+
+    expect(response.status).toBe(201);
+  });
+});
