dd80faf54c6a4fa3f19a0f46a007b2367a52a071
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-22 15:54:27 -0300

    test: add end-to-end test for updating a car

--- src/tests/routes/cars.test.ts
+++ src/tests/routes/cars.test.ts
@@ -60,4 +60,25 @@ describe("Cars Route E2E Tests", () => {
 
     expect(response.status).toBe(200);
   });
+
+  it("PATCH /api/cars/:carId should update a car successfully", async () => {
+    const { body: createdUser } = await request(app)
+      .post("/api/users")
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const { body: createdCar } = await request(app)
+      .post("/api/cars")
+      .set("Authorization", `Bearer ${createdUser.tokens.accessToken}`)
+      .send(car);
+
+    const response = await request(app)
+      .patch(`/api/cars/${createdCar.id}`)
+      .set("Authorization", `Bearer ${createdUser.tokens.accessToken}`)
+      .send({ brand: "Updated Brand", year: 2020 });
+
+    expect(response.status).toBe(200);
+  });
 });
