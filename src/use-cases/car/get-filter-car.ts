af3379d723ebc81c1683480d81bdcd5ebcc0c9e7
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-04-30 14:03:03 -0300

    feat: add GetFilterCarUseCase

---
+++ src/use-cases/car/get-filter-car.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { PostgresGetFilterCars } from "../../repositories/postgres/car/get-filter-car.js";
+
+export class GetFilterCarUseCase {
+  constructor(private getFilterCar: PostgresGetFilterCars) {}
+  async execute(filters: any) {
+    const cars = await this.getFilterCar.execute(filters);
+    return { cars };
+  }
+}
