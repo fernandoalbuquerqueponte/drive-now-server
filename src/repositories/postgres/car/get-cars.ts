50133c939e75065b58a5323e3837a5fd9810c88f
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-04 08:11:27 -0300

    feat: include specifications in GetCarRepository query

--- src/repositories/postgres/car/get-cars.ts
+++ src/repositories/postgres/car/get-cars.ts
@@ -2,7 +2,11 @@ import prismaClient from "../../../../prisma/prisma.js";
 
 export class GetCarRepository {
   async execute() {
-    const cars = await prismaClient.car.findMany();
+    const cars = await prismaClient.car.findMany({
+      include: {
+        specifications: true,
+      },
+    });
 
     return cars;
   }
