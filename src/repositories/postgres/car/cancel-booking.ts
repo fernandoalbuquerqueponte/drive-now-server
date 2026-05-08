8561bdec41f843e4731ed5d544f9fab4ef51663d
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-07 21:45:32 -0300

    feat: add PostgresCancelBooking

---
+++ src/repositories/postgres/car/cancel-booking.ts
@@ -0,0 +1,14 @@
+import prismaClient from "../../../../prisma/prisma.js";
+
+export class PostgresCancelBooking {
+  async execute(bookingId: string) {
+    return await prismaClient.booking.update({
+      where: {
+        id: bookingId,
+      },
+      data: {
+        status: "CANCELLED",
+      },
+    });
+  }
+}
