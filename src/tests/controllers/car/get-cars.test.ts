30dd167587fa87e943c23230b9c0ccb74fd845df
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-08 17:29:34 -0300

    feat: enhance GetCarsController tests to validate query parameters and responses

--- src/tests/controllers/car/get-cars.test.ts
+++ src/tests/controllers/car/get-cars.test.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { GetCarsUseCase } from "../../../use-cases/car/get-cars.js";
 import { GetCarsController } from "../../../controllers/car/get-cars.js";
 import { car } from "../../fixtures/car.js";
+import { faker } from "@faker-js/faker";
 
 describe("GetCarsController", () => {
   class GetCarsUseCaseStub {
@@ -21,12 +23,30 @@ describe("GetCarsController", () => {
     };
   };
 
+  const httpRequest = {
+    query: {
+      search: faker.vehicle.model(),
+      category: "SUV",
+      priceRange: "R$ 50 - R$ 100/h",
+      transmission: "Automático",
+      fuel: "Gasolina",
+    },
+  };
+
   it("should get cars successfully", async () => {
     const { sut } = makeSut();
 
-    const bookings = await sut.execute();
+    const bookings = await sut.execute(httpRequest);
 
     expect(bookings.statusCode).toBe(200);
+  });
+
+  it("should return 200 if query is not provided", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({});
+
+    expect(response.statusCode).toBe(200);
   });
 
   it("should return 500 if throws generic error", async () => {
@@ -35,8 +55,68 @@ describe("GetCarsController", () => {
       throw new Error();
     });
 
-    const result = await sut.execute();
+    const result = await sut.execute(httpRequest);
 
     expect(result.statusCode).toBe(500);
+  });
+
+  it("should return 400 if search is not a string", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      query: {
+        search: 12345 as any,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 400 if category is not a string", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      query: {
+        category: 12345 as any,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 400 if priceRange is not a string", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      query: {
+        priceRange: 12345 as any,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 400 if transmission is not a string", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      query: {
+        transmission: 12345 as any,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 400 if fuel is not a string", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.execute({
+      query: {
+        fuel: 12345 as any,
+      },
+    });
+
+    expect(response.statusCode).toBe(400);
   });
 });
