d5d2a16bfeec5934168286c9179063097ba0c6f5
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-09 01:43:52 -0300

    test: add case to return 500 if a generic error is thrown in AddCarReviewController

--- src/tests/controllers/car/add-car-review.test.ts
+++ src/tests/controllers/car/add-car-review.test.ts
@@ -138,4 +138,15 @@ describe("AddCarReviewController", () => {
 
     expect(response.statusCode).toBe(404);
   });
+
+  it("should return 500 if throw generic error", async () => {
+    const { sut, addCarReviewUseCase } = makeSut();
+    jest
+      .spyOn(addCarReviewUseCase, "execute")
+      .mockRejectedValueOnce(new Error());
+
+    const response = await sut.execute(httpRequest);
+
+    expect(response.statusCode).toBe(500);
+  });
 });
