6ac51fe608974a20e4cf91430e2de2eb4df6ac8b
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-09 02:28:17 -0300

    test: add additional test to verify prisma call parameters in PostgresGetBookingsByCarIdRepository

--- src/tests/repositories/car/get-bookings-by-car-id.test.ts
+++ src/tests/repositories/car/get-bookings-by-car-id.test.ts
@@ -19,4 +19,31 @@ describe("PostgresGetBookingsByCarIdRepository", () => {
 
     expect(result).toBeTruthy();
   });
+
+  it("should call prisma with correct params", async () => {
+    const createdUser = await prismaClient.user.create({ data: user });
+    const createdCar = await prismaClient.car.create({
+      data: {
+        ...buildPrismaCarData(car),
+        user_id: createdUser.id,
+      },
+    });
+
+    const sut = new PostgresGetBookingsByCarIdRepository();
+
+    const prismaSpy = jest.spyOn(prismaClient.booking, "findMany");
+
+    await sut.execute(createdCar.id);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        carId: createdCar.id,
+        status: { in: ["CONFIRMED", "PENDING"] },
+      },
+      select: {
+        startDate: true,
+        endDate: true,
+      },
+    });
+  });
 });
