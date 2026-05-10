0d1780fa8389fb4806b1942140ecbce3de8d92c1
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-09 23:51:13 -0300

    test: add GetBookingsCarByUserIdUseCase with successful user id booking retrieval

---
+++ src/tests/use-cases/car/get-bookings-car-by-user-id.test.ts
@@ -0,0 +1,44 @@
+import type { Booking } from "@prisma/client";
+import { booking } from "../../fixtures/car.js";
+import { faker } from "@faker-js/faker";
+import { GetBookingsCarByUserIdUseCase } from "../../../use-cases/car/get-bookings-car-by-user-id.js";
+import type { PostgresGetBookingsCarByUserId } from "../../../repositories/postgres/car/get-bookings-car-by-user-id.js";
+
+describe("GetBookingsCarByUserIdUseCase", () => {
+  const userId = faker.string.uuid();
+
+  class GetBookingsCarByUserIdRepositoryStub {
+    async execute(): Promise<Booking[]> {
+      return [
+        {
+          ...booking,
+          userId: userId,
+        },
+      ] as Booking[];
+    }
+  }
+
+  const makeSut = () => {
+    const getBookingsCarByUserId =
+      new GetBookingsCarByUserIdRepositoryStub() as unknown as PostgresGetBookingsCarByUserId;
+    const sut = new GetBookingsCarByUserIdUseCase(getBookingsCarByUserId);
+
+    return {
+      sut,
+      getBookingsCarByUserId,
+    };
+  };
+
+  it("should get bookings by user id successfully", async () => {
+    const { sut } = makeSut();
+
+    const result = await sut.execute(userId);
+
+    expect(result).toEqual([
+      {
+        ...booking,
+        userId: userId,
+      },
+    ]);
+  });
+});
