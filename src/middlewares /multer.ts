968848a4b9f5bdd02e989a6a814d07bcff07fd8a
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-17 19:52:38 -0300

    fix: ensure consistent formatting in multer configuration

--- src/middlewares /multer.ts
+++ src/middlewares /multer.ts
@@ -12,6 +12,6 @@ const storage = multer.diskStorage({
 });
 
 export const uploadCarImages = multer({ storage }).fields([
-  { name: "image", maxCount: 1 },    // Apenas 1 imagem principal
+  { name: "image", maxCount: 1 }, // Apenas 1 imagem principal
   { name: "gallery", maxCount: 20 }, // Até 20 imagens na galeria
 ]);
