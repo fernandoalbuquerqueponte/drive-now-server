7fd6f42bc53222fe9af6ea1ef161b1ba9aa7ac16
Fernando de Albuquerque <ffernandoalbuquerqueponte@gmail.com>
2026-05-25 14:28:58 -0300

    refactor: clean up comments in auth middleware for clarity

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,26 +7,22 @@ export const auth = (
   next: NextFunction,
 ) => {
   try {
-    // pegar o access token do Header
     const accessToken = request.headers?.authorization?.split("Bearer ")[1];
     if (!accessToken) {
       return response.status(401).send({ message: "Unauthorized" });
     }
 
-    //Verificar se o access token é válido
     const decodedToken = jwt.verify(
       accessToken,
-      process.env.JWT_ACCESS_TOKEN_SECRET!, //SE O TOKEN FOI GERADO POR VC E TEM A CHAVE SECRETA, ELE É VÁLIDO
-    ) as { userId: string }; // SE FOR VÁLIDO ELE RETORNA O PAYLOAD
+      process.env.JWT_ACCESS_TOKEN_SECRET!,
+    ) as { userId: string };
 
     if (!decodedToken) {
       return response.status(401).send({ message: "Unauthorized" });
     }
 
     request.userId = decodedToken.userId;
-    //Se for válido, deixar a requisição continuar
     next();
-    //Se não for válido, retornar um erro 401
   } catch (error) {
     console.error(error);
     return response.status(401).send({ message: "Unauthorized" });
